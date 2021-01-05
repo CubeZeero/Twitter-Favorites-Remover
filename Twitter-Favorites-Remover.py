c4c1cdf05db18963ace8d427a86b7c03e327aa7f
Cube <48977674+CubeZeero@users.noreply.github.com>
2021-01-06 01:43:34 +0900

    Add files via upload

---
+++ Twitter-Favorites-Remover.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+import tweepy
+import webbrowser
+import sys
+import time
+import math
+from tqdm import tqdm
+
+consumer_key = ''
+consumer_secret = ''
+callback_url = 'oob'
+
+tweet_count = 0
+tweet_count_progress = 0
+tweet_count_progress_limit = 0
+favorites_count_tmp = 0
+pages = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_url)
+
+auth_url = auth.get_authorization_url()
+webbrowser.open(auth_url)
+
+verifier = input('\nPlease authenticate your account and get a PIN\nアカウントを認証してPINを取得してください\nPIN: ').strip()
+auth.get_access_token(verifier)
+
+auth.set_access_token(auth.access_token, auth.access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_delay=10800)
+screen_name = api.me().screen_name
+print('\nThe following users have been authenticated\n以下のユーザーが認証されました\n@' + screen_name + '\n')
+
+user=api.get_user(screen_name = screen_name)
+print('Number of favorite\nいいね数\n' + str(user.favourites_count))
+favorites_count_tmp = user.favourites_count
+
+tweet_count_progress_limit = math.floor(user.favourites_count/100)
+
+print('\n*!W A R N I N G!*\nAre you sure you want to remove all your favorites?\nいいねをすべて削除しますがよろしいですか？')
+yorn = input('y/n\n').strip()
+
+if yorn == 'n':
+    print('\nExit the tool\nツールを終了します')
+    time.sleep(2)
+    sys.exit()
+
+progress_bar = tqdm(total = favorites_count_tmp)
+progress_bar.set_description('Removing... ')
+
+while favorites_count_tmp > 0:
+
+    for page in pages:
+        tweets=api.favorites(screen_name=screen_name, count=200, page=page)
+
+        for tweet in tweets:
+            tweet_count = tweet_count + 1
+            tweet_count_progress = tweet_count_progress + 1
+
+            while True:
+                try:
+                    api.destroy_favorite(tweet.id)
+                    break
+                except TimeoutError:
+                    pass
+
+            progress_bar.update(1)
+            time.sleep(0.05)
+
+    user = api.get_user(screen_name = screen_name)
+    favorites_count_tmp = user.favourites_count
+
+print('\nComplete\n削除が完了しました')
+os.system('PAUSE')
+print('\nExit the tool\nツールを終了します')
+time.sleep(3)
